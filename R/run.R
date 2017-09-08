7cb808510e9d314751e428bc905319acc86f2d1a
Robrecht Cannoodt <brechtus@gmail.com>
2017-09-08 09:59:52 +0200

    code is compiling. will hopefully run on travis.

--- R/run.R
+++ R/run.R
@@ -1,20 +1,13 @@
-#' #' A simple wrapper for the GEDEVO executables
-#' #'
-#' #' @param args a string containing the command that would normally be
-#' #'   entered for running the GEDEVO executable.
-#' #' @param verbose whether or not to print the GEDEVO output
-#' #'
-#' #' @examples
-#' #' \dontrun{
-#' #'   cmd <- paste0(
-#' #'     "./gedevo --resume test.state --create --groups DM hprd --ntw DM.ntw "
-#' #'     "--ntw hprd.ntw --grsig hprd.sigs hprd --grsig DM.sigs DM --undirected "
-#' #'     "--pop 1000 --maxsame 500 --no-prematch")
-#' #'   run_GEDEVO(cmd)
-#' #' }
-#' #'
-#' #' @export
-#' run_GEDEVO <- function(args_string, verbose = F) {
-#'   args <- strsplit(args_string, " ")[[1]]
-#'   poor_mans_wrap(args, verbose)
-#' }
+#' A simple wrapper for the SCOUP executable
+#'
+#' @param method which executable to run, can be one of \code{"scoup"}, \code{"scoup_resume"}, \code{"sp"}, or \code{"correlation"}.
+#' @param args a string containing the command that would normally be
+#'   entered for running the SCOUP executable.
+#' @param verbose whether or not to print the SCOUP output
+#'
+#' @export
+run_SCOUP <- function(method, args_string, verbose = F) {
+  args <- strsplit(args_string, " ")[[1]]
+  method_int <- c("scoup", "scoup_resume", "sp", "correlation")[method]
+  main_wrap(args, verbose, method_int)
+}
